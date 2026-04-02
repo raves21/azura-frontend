73d9f5637b6ca80bece6f942f705d18fed1a7f90
raves21 <johnraves23@gmail.com>
2026-04-03 02:16:27 +0800

    chore: commented out azura main in tvmovie server selection dropdown

--- src/utils/variables/media/shared.ts
+++ src/utils/variables/media/shared.ts
@@ -1,7 +1,7 @@
 import { TVMovieServerName } from "@/utils/types/media/shared";
 
 export const tvMovieserverNames: TVMovieServerName[] = [
-  TVMovieServerName.azuraMain,
+  // TVMovieServerName.azuraMain,
   TVMovieServerName.embed1,
   TVMovieServerName.embed2,
 ];
