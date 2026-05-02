8727ee714f3aefee940a9dc85adf664ddb63f74d
arib <johnraves23@gmail.com>
2026-05-03 03:45:04 +0800

    Change sort property to format an array of sorts

--- src/services/media/anime/queries.ts
+++ src/services/media/anime/queries.ts
@@ -134,7 +134,7 @@ export function useFilterAnime({
               ? `[${genres?.map((genre) => `"${genre}"`)}]`
               : undefined,
             year,
-            sort: sortBy,
+            sort: `[${sortBy.map((sort) => `"${sort}"`)}]`,
             format,
             page: page ?? 1,
             status,
